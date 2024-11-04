39c295d23b7ca3e0128f70b581ae59b183b87112
Al168815 <116653962+Al168815@users.noreply.github.com>
2024-11-04 10:05:21 -0700

    Cell View 4 noviembre

---
+++ bocetos_2/Bocetos_2/controladores_de_pantalla/vista_de_celdas/VistaDeComentario.swift
@@ -0,0 +1,14 @@
+//
+//  VistaDeComentario.swift
+//  boceto_2_CellView
+//
+//  Created by alumno on 10/28/24.
+//
+
+import UIKit
+
+class VistaDeComentario: UICollectionViewCell{
+    
+    @IBOutlet weak var comentario: UILabel!
+    
+}
